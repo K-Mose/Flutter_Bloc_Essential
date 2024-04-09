9d972c454ce85967e8167a58f1075bf490638bf8
Mose <km_413@naver.com>
2024-04-09 14:17:21 +0900

    Bloc ToDoApp - 1. Computed State: FilteredTodos Cubit 2. cubit barrel file

---
+++ todo_app/lib/cubits/cubits.dart
@@ -0,0 +1,6 @@
+// Cubit의 개수가 너무 많아서 하나로 묶어 외부에서 import 할 때 편리하게 사용
+export 'todo_filter/todo_filter_cubit.dart';
+export 'todo_list/todo_list_cubit.dart';
+export 'todo_search/todo_search_cubit.dart';
+export 'active_todo_count/active_todo_count_cubit.dart';
+export 'filtered_todos/filtered_todos_cubit.dart';
