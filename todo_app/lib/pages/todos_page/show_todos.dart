61fd689c643e7466d547f7291608f55e09648811
Mose <km_413@naver.com>
2024-04-11 10:48:24 +0900

    Bloc ToDoApp - 1. TodoItem Widget

--- todo_app/lib/pages/todos_page/show_todos.dart
+++ todo_app/lib/pages/todos_page/show_todos.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:todo_app/cubits/cubits.dart';
+import 'package:todo_app/models/todo_model.dart';
 
 class ShowTodos extends StatelessWidget {
   const ShowTodos({Key? key}) : super(key: key);
@@ -21,10 +22,33 @@ class ShowTodos extends StatelessWidget {
           key: ValueKey(todos[index].id),
           background: showBackground(0),
           secondaryBackground: showBackground(1),
-          child: Text(
-            todos[index].desc,
-            style: TextStyle(fontSize: 20.0),
-          ),
+          onDismissed: (_) {
+            context.read<TodoListCubit>().removeTodo(todos[index]);
+          },
+          // 삭제 시 확인
+          confirmDismiss: (_) {
+            return showDialog(
+              context: context,
+              barrierDismissible: false,
+              builder: (context) {
+                return AlertDialog(
+                  title: const Text('Are you sure?'),
+                  content: const Text('Do you really want to delete?'),
+                  actions: [
+                    TextButton(
+                      onPressed: () => Navigator.of(context).pop(false),
+                      child: const Text('NO'),
+                    ),
+                    TextButton(
+                      onPressed: () => Navigator.of(context).pop(true),
+                      child: const Text('YES'),
+                    ),
+                  ],
+                );
+              },
+            );
+          },
+          child: TodoItem(todo: todos[index],),
         );
       },
     );
@@ -41,6 +65,81 @@ class ShowTodos extends StatelessWidget {
         size: 30.0,
         color: Colors.white,
       ),
+    );
+  }
+}
+
+class TodoItem extends StatefulWidget {
+  final Todo todo;
+  const TodoItem({super.key, required this.todo});
+
+  @override
+  State<TodoItem> createState() => _TodoItemState();
+}
+
+class _TodoItemState extends State<TodoItem> {
+  late final TextEditingController tcEdit;
+
+  @override
+  void initState() {
+    super.initState();
+    tcEdit = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    tcEdit.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      onTap: () {
+        showDialog(context: context, builder: (context) {
+          bool _error = false;
+          tcEdit.text = widget.todo.desc;
+          // ShowDialog는 같은 트래 내 위젯이 아니고 AlertDialog가 stateless 위젯이라서ㅓ
+          // StatefulBuilder로 감싸준다.
+          return StatefulBuilder(builder: (context, setState) {
+            return AlertDialog(
+              title: Text('Edit Todo'),
+              content: TextField(
+                controller: tcEdit,
+                autofocus: true,
+                decoration: InputDecoration(
+                  errorText: _error ? "Value cannot be empty" : null
+                ),
+              ),
+              actions: [
+                TextButton(
+                  onPressed: () => Navigator.of(context).pop(),
+                  child: const Text('CANCEL'),
+                ),
+                TextButton(
+                  onPressed: () {
+                    setState(() {
+                      _error = tcEdit.text.isEmpty ? true : false;
+                      if (!_error) {
+                        context.read<TodoListCubit>().editTodo(widget.todo.id, tcEdit.text);
+                        Navigator.of(context).pop();
+                      }
+                    });
+                  },
+                  child: const Text('EDIT'),
+                ),
+              ],
+            );
+          },);
+        },);
+      },
+      leading: Checkbox(
+        value: widget.todo.completed,
+        onChanged: (bool? checked) {
+          context.read<TodoListCubit>().toggleTodo(widget.todo.id);
+        }
+      ),
+      title: Text(widget.todo.desc),
     );
   }
 }
