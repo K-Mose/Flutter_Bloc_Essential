94efddf15b03396be81a4a06eb9a56d0573762cc
Mose <km_413@naver.com>
2024-04-15 14:40:16 +0900

    Bloc ToDoApp - 1. Bloc Project : FilteredTodosBloc

---
+++ 1_todo_app/todo_app_bloc/lib/blocs/filtered_todos/filtered_todos_state.dart
@@ -0,0 +1,25 @@
+part of 'filtered_todos_bloc.dart';
+
+class FilteredTodosState extends Equatable {
+  final List<Todo> filteredTodos;
+  const FilteredTodosState({required this.filteredTodos});
+
+  factory FilteredTodosState.initial({List<Todo>? todos}) =>
+      FilteredTodosState(filteredTodos: todos ?? []);
+
+  @override
+  List<Object> get props => [filteredTodos];
+
+  @override
+  String toString() {
+    return 'FilteredTodosState{filteredTodos: $filteredTodos}';
+  }
+
+  FilteredTodosState copyWith({
+    List<Todo>? filteredTodos,
+  }) {
+    return FilteredTodosState(
+      filteredTodos: filteredTodos ?? this.filteredTodos,
+    );
+  }
+}
