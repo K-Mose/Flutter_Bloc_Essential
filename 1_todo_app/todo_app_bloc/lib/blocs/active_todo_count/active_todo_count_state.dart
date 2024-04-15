39c490f8257fce222245369a3982759f2f488104
Mose <km_413@naver.com>
2024-04-15 14:27:04 +0900

    Bloc ToDoApp - 1. Bloc Project : ActiveTodoCountBloc

---
+++ 1_todo_app/todo_app_bloc/lib/blocs/active_todo_count/active_todo_count_state.dart
@@ -0,0 +1,30 @@
+part of 'active_todo_count_bloc.dart';
+
+class ActiveTodoCountState extends Equatable {
+  final int activeTodoCount;
+
+  const ActiveTodoCountState({
+    required this.activeTodoCount,
+  });
+
+
+  factory ActiveTodoCountState.initial() =>
+      // TodoListState에 3개의 초기 Todo가 있으므로 0은 정확한 초기값이 아님
+  const ActiveTodoCountState(activeTodoCount: 0);
+
+  @override
+  List<Object> get props => [];
+
+  @override
+  String toString() {
+    return 'ActiveTodoCountState{activeTodoCount: $activeTodoCount}';
+  }
+
+  ActiveTodoCountState copyWith({
+    int? activeTodoCount,
+  }) {
+    return ActiveTodoCountState(
+      activeTodoCount: activeTodoCount ?? this.activeTodoCount,
+    );
+  }
+}
